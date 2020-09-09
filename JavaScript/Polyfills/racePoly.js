22189413835072ecbc381fa76f6ef1b405b29d94
GigaGeekKPI <life.x.dream.programmer@gmail.com>
2020-09-09 14:16:42 +0300

    fix/racePoly

--- JavaScript/Polyfills/racePoly.js
+++ JavaScript/Polyfills/racePoly.js
@@ -1,17 +1,16 @@
 Promise.racePoly = function (promises) {
     return new Promise(function (resolve, reject) {
-        for (var i = 0; i < promises.length; i++) {
-            promises[i].then(function (result) {
-                resolve(result);
-            }, function (err) {
-                reject(err)
-            });
-        }
+        promises.forEach(function (promise) {
+            if (!(promise instanceof Promise)) {
+                resolve(promise);
+            }
+            promise.then(resolve, reject);
+        });
     });
 };
 
 Promise.racePoly([
     new Promise((resolve, reject) => setTimeout(() => reject('Error'), 3000)), // 1
     new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
-    new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
+    new Promise(resolve => setTimeout(() => resolve(3), 1000)),  // 3
 ]).then(console.log).catch(console.log);
